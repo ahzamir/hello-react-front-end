490041c700e36ef260cbf2cba7b990b6fd224104
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-27 22:29:36 +0430

    add some redux code to fetch greetings from api endpoints and created reducer for greeting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import { Provider } from 'react-redux';
+import store from './components/redux/configureStore';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
